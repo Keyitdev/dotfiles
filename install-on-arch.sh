e57adebd634bf994b2439ac0dc0e37bdd40bc3a4
smartsyncing <yes.vrocht@slmail.me>
2022-02-20 14:40:29 +0000

    Updated the installer to work properly and more efficiently

--- install-on-arch.sh
+++ install-on-arch.sh
@@ -49,7 +49,17 @@ aur_helper(){
 install_packages(){
     echo -e "[*3*] Installng packages with Pacman."
     sudo pacman -S --noconfirm --needed light pulseaudio pulseaudio-alsa pulsemixer alsa-utils pacman-contrib i3-gaps i3blocks xorg xorg-xinit xorg-server feh imagemagick kitty rofi dunst libnotify ranger ncmpcpp mpd papirus-icon-theme btop sddm zsh picom code neovim xclip scrot mpc
-    
+    echo -e "\nInstalling other dependencies"
+    git clone --depth 10 https://github.com/kabinspace/AstroVim.git ~/.config/nvim
+    nvim +PackerSync
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/EliverLara/ant.git ~/ant
+    sudo mv ~/ant /usr/share/themes
+    git clone https://github.com/keyitdev/sddm-astronaut-theme.git ~/sddm-astronaut-theme
+    sudo cp -fdr ~/sddm-astronaut-theme /usr/share/sddm/themes/
+    sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+    sudo echo "[Theme]
+Current=sddm-astronaut-theme" >> /etc/sddm.conf
     echo -e "\n[*] Installing packages with $HELPER."
     $HELPER -S acpi      \
 	   polybar           \
@@ -66,17 +76,17 @@ install_packages(){
     cat <<- EOF
 		[*3*] Still installing packages.
 		
-		[*] Do you want to install optional, but useful programs? (VSCode, iwd, LibreOffice, Firefox, etc)
+		[*] Do you want to install optional, but useful programs? (VSCode, iwd, Oh-My-Zsh, LibreOffice, Firefox, etc)
 
 		[1] yes
-		[2] nope
+		[2] no
 	
 	EOF
 
 	read -p "[?] Select option: "
 
 	if [[ $REPLY == "1" ]]; then
-		sudo pacman -S --noconfirm --needed code iwd dhcpcd ntfs-3g libreoffice firefox nautilus gimp
+		sudo pacman -S --noconfirm --needed code iwd dhcpcd ntfs-3g libreoffice firefox nautilus gimp 
 	elif [[ $REPLY == "2" ]]; then
 		 echo -e "\n[*] Skipping."
 	else
